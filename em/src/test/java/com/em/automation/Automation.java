c6cc7ff02f42a989f93738e140c38a0282b55b1c
olegzaimm <olegzaimm@10.33.11.118>
2019-03-02 15:39:01 +0200

    Fixed broken tests and added new test case

--- em/src/test/java/com/em/automation/Automation.java
+++ em/src/test/java/com/em/automation/Automation.java
@@ -33,8 +33,36 @@ public class Automation {
 		
 		Thread.sleep(5000); // Let the user actually see something!
 		driver.quit();
-		// Close the instance of browser
-		driver.close();
+	}
+	
+	@Test
+	public void testLoginPageInvalidPasswordAndUsername() throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "/Users/olegzaimm/Downloads/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8080/login");
+		Thread.sleep(5000); // Let the user actually see something!
+		WebElement usernameElement = driver.findElement(By.name("username"));
+		WebElement passElement = driver.findElement(By.name("password"));
+		WebElement formElement = driver.findElement(By.id("loginForm"));
+		
+		
+		usernameElement.sendKeys("oleg123");
+		passElement.sendKeys("oleg23344");
+		
+		formElement.submit();
+		
+		  WebDriverWait wait = new WebDriverWait(driver, 10);
+		    WebElement messageElement = wait.until(
+		           ExpectedConditions.presenceOfElementLocated(By.id("error"))
+		            );
+		 String message                 = messageElement.getText();
+		    String successMsg             = "The username or password is incorrect!";
+		    Assert.assertEquals (message, successMsg);
+		
+		Thread.sleep(5000); // Let the user actually see something!
+		driver.quit();
 	}
 
 }
